55860e5f18437588370ac4c7576b5ce6e7a4bf37
Bram Walet <bramwalet@users.noreply.github.com>
2014-07-16 22:26:18 +0200

    Implemented find best subtitles, enabling/disabling providers, provider settings
    Enabled dogpile.cache to be in memory to make tvsubtitles and addic7ed work.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,4 +1,4 @@
-#hdbits.org
+# hdbits.org
 
 import string, os, urllib, zipfile, re, copy
 from babelfish import Language
@@ -7,38 +7,54 @@ import subliminal
 import logger
 
 OS_PLEX_USERAGENT = 'plexapp.com v9.0'
-subtitleExt       = ['utf','utf8','utf-8','sub','srt','smi','rt','ssa','aqt','jss','ass','idx']
-
-langPrefs2Podnapisi = {'sq':'29','ar':'12','be':'50','bs':'10','bg':'33','ca':'53','zh':'17','cs':'7','da':'24','nl':'23','en':'2','et':'20','fi':'31','fr':'8','de':'5','el':'16','he':'22','hi':'42','hu':'15','is':'6','id':'54','it':'9','ja':'11','ko':'4','lv':'21','lt':'19','mk':'35','ms':'55','no':'3','pl':'26','pt':'32','ro':'13','ru':'27','sr':'36','sk':'37','sl':'1','es':'28','sv':'25','th':'44','tr':'30','uk':'46','vi':'51','hr':'38'}
-
-mediaCopies = {}
 
 DEPENDENCY_MODULE_NAMES = ['subliminal', 'enzyme', 'guessit', 'requests']
+
+SUPPORTED_PROVIDERS = ['opensubtitles', 'thesubdb', 'podnapisi', 'addic7ed', 'tvsubtitles']
+
+ENABLED_PROVIDERS = {'opensubtitles' : Prefs['provider.opensubtitles'],
+                     'thesubdb' : Prefs['provider.thesubdb'],
+                     'podnapisi' : Prefs['provider.podnapisi'],
+                     'addic7ed' : Prefs['provider.addic7ed'],
+                     'tvsubtitles' : Prefs['provider.tvsubtitles']
+                     }
+# dict((key,value) for key, value in a.iteritems() if key == 1)
+SUPPORTED_PROVIDER_SETTINGS = {'addic7ed': { 
+                                            'username': Prefs['provider.addic7ed.username'], 
+                                            'password': Prefs['provider.addic7ed.password']
+                                            }
+                               }
 
 def Start():
     HTTP.CacheTime = 0
     HTTP.Headers['User-agent'] = OS_PLEX_USERAGENT
-    Log.Debug("START  CALLED")
+    Log.Debug("START CALLED")
     logger.registerLoggingHander(DEPENDENCY_MODULE_NAMES)
+    # configured cache to be in memory as per https://github.com/Diaoul/subliminal/issues/303
+    subliminal.cache_region.configure('dogpile.cache.memory')
 
 def ValidatePrefs():
     Log.Debug("Validate Prefs called.")
     return 
 
-#Prepare a list of languages we want subs for
+# Prepare a list of languages we want subs for
 def getLangList():
-    langList = [Prefs["langPref1"]]
+    langList = {Language.fromietf(Prefs["langPref1"])}
     if(Prefs["langPref2"] != "None"):
-        langList.append(Prefs["langPref2"])
+        langList.update({Language.fromietf(Prefs["langPref2"])})
+        
     return langList
 
-class SubInfo():
-    def __init__(self, lang, url, sub, name):
-        self.lang = lang
-        self.url = url
-        self.sub = sub
-        self.name = name
-        self.ext = string.split(self.name, '.')[-1]
+def getProviders():
+    enabledProviders = []
+    for supportedProvider in SUPPORTED_PROVIDERS:
+        if Prefs["provider." + supportedProvider]:
+            Log.Debug("Provider %s is enabled" % supportedProvider)
+            enabledProviders.append(supportedProvider)
+    return enabledProviders
+
+def getProviderSettings():
+    return SUPPORTED_PROVIDER_SETTINGS
 
 class SubliminalSubtitlesAgentMovies(Agent.Movies):
     name = 'Subliminal Movie Subtitles'
@@ -48,15 +64,27 @@ class SubliminalSubtitlesAgentMovies(Agent.Movies):
 
     def search(self, results, media, lang):
         Log.Debug("MOVIE SEARCH CALLED")
-        
+        results.Append(MetadataSearchResult(id='null', score=100))
+
 
     def update(self, metadata, media, lang):
         Log.Debug("MOVIE UPDATE CALLED")
+        for item in media.items:
+            for part in item.parts:
+                Log.Debug("Append: %s" % part.file)
+                try:
+                    scannedVideo = subliminal.video.scan_video(part.file, subtitles=True, embedded_subtitles=True)
+                except ValueError:
+                    Log.Warn("File could not be guessed by subliminal")
+                    continue
+                
+                videos.append(scannedVideo)
 
+        subtitles = subliminal.api.download_best_subtitles(videos, getLangList(), getProviders(), getProviderSettings())
+        subliminal.api.save_subtitles(subtitles)
 
 class SubliminalSubtitlesAgentTvShows(Agent.TV_Shows):
     
-    providers = ['opensubtitles']
     name = 'Subliminal TV Subtitles'
     languages = [Locale.Language.English]
     primary_provider = False
@@ -64,11 +92,10 @@ class SubliminalSubtitlesAgentTvShows(Agent.TV_Shows):
 
     def search(self, results, media, lang):
         Log.Debug("TV SEARCH CALLED")
-        results.Append(MetadataSearchResult(id = 'null', score = 100))
-
+        results.Append(MetadataSearchResult(id='null', score=100))
         
     def update(self, metadata, media, lang):
-        videos= []
+        videos = []
         Log.Debug("TvUpdate. Lang %s" % lang)
         for season in media.seasons:
             for episode in media.seasons[season].episodes:
@@ -76,13 +103,12 @@ class SubliminalSubtitlesAgentTvShows(Agent.TV_Shows):
                     for part in item.parts:
                         Log.Debug("Append: %s" % part.file)
                         try:
-                            scannedVideo = subliminal.video.scan_video(part.file)
+                            scannedVideo = subliminal.video.scan_video(part.file, subtitles=True, embedded_subtitles=True)
                         except ValueError:
                             Log.Warn("File could not be guessed by subliminal")
                             continue
                         
                         videos.append(scannedVideo)
-                     
-        result = subliminal.api.list_subtitles(videos,{Language('eng')}, self.providers)
-        Log.Debug(result)
-        
+
+        subtitles = subliminal.api.download_best_subtitles(videos, getLangList(), getProviders(), getProviderSettings())
+        subliminal.api.save_subtitles(subtitles)
