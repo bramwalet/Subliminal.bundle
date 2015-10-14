520cbb5189b9195ffebad7466f5b53ff30cb51e9
panni <panni@fragstore.net>
2015-10-14 14:37:44 +0200

    patch subtitle repr to include download/page link; fixes #34

--- Sub-Zero.bundle/Contents/Libraries/Shared/subliminal_patch/__init__.py
+++ Sub-Zero.bundle/Contents/Libraries/Shared/subliminal_patch/__init__.py
@@ -11,11 +11,24 @@ from .patch_providers import addic7ed, podnapisi, tvsubtitles, opensubtitles
 # patch subliminal's ProviderPool 
 subliminal.api.ProviderPool = PatchedProviderPool
 
+# patch subliminal's subtitle classes
+def subtitleRepr(self):
+    link = self.page_link
+
+    # specialcasing addic7ed; eww
+    if self.__class__.__name__ == "Addic7edSubtitle":
+	link = u"http://www.addic7ed.com/%s" % self.download_link
+    return '<%s %r [%s]>' % (self.__class__.__name__, link, self.language)
+
+subliminal.subtitle.Subtitle.__repr__ = subtitleRepr
+
 # patch subliminal's providers
 subliminal.providers.addic7ed.Addic7edProvider = addic7ed.PatchedAddic7edProvider
 subliminal.providers.podnapisi.PodnapisiProvider = podnapisi.PatchedPodnapisiProvider
 subliminal.providers.tvsubtitles.TVsubtitlesProvider = tvsubtitles.PatchedTVsubtitlesProvider
 subliminal.providers.opensubtitles.OpenSubtitlesProvider = opensubtitles.PatchedOpenSubtitlesProvider
+
+
 
 # add language converters
 babelfish.language_converters.register('addic7ed = subliminal_patch.patch_language:PatchedAddic7edConverter')
--- Sub-Zero.bundle/Contents/Libraries/Shared/subliminal_patch/patch_providers/mixins.py
+++ Sub-Zero.bundle/Contents/Libraries/Shared/subliminal_patch/patch_providers/mixins.py
@@ -2,6 +2,11 @@
 
 
 class PunctuationMixin(object):
+    """
+    provider mixin
+
+    fixes show ids for stuff like "Mr. Petterson", as our matcher already sees it as "Mr Petterson" but addic7ed doesn't
+    """
     def clean_punctuation(self, s):
-        # fixes show ids for stuff like "Mr. Petterson", as our matcher already sees it as "Mr Petterson" but addic7ed doesn't
 	return s.replace(".", "").replace(":", "").replace("'", "")
+
