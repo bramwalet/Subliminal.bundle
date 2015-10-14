e8eeadb094ee06435ab928908823f0463da3550d
panni <panni@fragstore.net>
2015-10-14 13:57:27 +0200

    add colon and single quote to punctuation fix mixin; resolves #36

--- Sub-Zero.bundle/Contents/Libraries/Shared/subliminal_patch/patch_providers/mixins.py
+++ Sub-Zero.bundle/Contents/Libraries/Shared/subliminal_patch/patch_providers/mixins.py
@@ -4,4 +4,4 @@
 class PunctuationMixin(object):
     def clean_punctuation(self, s):
         # fixes show ids for stuff like "Mr. Petterson", as our matcher already sees it as "Mr Petterson" but addic7ed doesn't
-	return s.replace(".", "")
+	return s.replace(".", "").replace(":", "").replace("'", "")
