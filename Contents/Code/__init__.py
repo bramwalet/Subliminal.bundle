21285c2f541de0fee5bcff19f7490f33c2860d2c
panni <panni@fragstore.net>
2015-10-22 18:33:00 +0200

    declutter __init__.py; move custom configuration stuff into subzero/config.py#Config()

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -10,12 +10,12 @@ import subliminal_patch
 import subzero
 import logger
 
-from babelfish import Language
 from datetime import timedelta
 from subzero.recent_items import getRecentItems
 from subzero.background import DefaultScheduler
 
 from subzero.subtitlehelpers import getSubtitlesFromMetadata
+from subzero.config import config
 
 OS_PLEX_USERAGENT = 'plexapp.com v9.0'
 
@@ -30,6 +30,9 @@ def Start():
     # configured cache to be in memory as per https://github.com/Diaoul/subliminal/issues/303
     subliminal.region.configure('dogpile.cache.memory')
 
+    # init defaults; perhaps not the best idea to use ValidatePrefs here, but we'll see
+    ValidatePrefs()
+
     #recent_items = getRecentItems()
     #print recent_items
     scheduler = DefaultScheduler()
@@ -38,67 +41,15 @@ def Start():
 
 def ValidatePrefs():
     Log.Debug("Validate Prefs called.")
+    config.initialize()
     return 
-
-# Prepare a list of languages we want subs for
-def getLangList():
-    langList = {Language.fromietf(Prefs["langPref1"])}
-    langCustom = Prefs["langPrefCustom"].strip()
-
-    if Prefs["langPref2"] != "None":
-        langList.update({Language.fromietf(Prefs["langPref2"])})
-
-    if Prefs["langPref3"] != "None":
-        langList.update({Language.fromietf(Prefs["langPref3"])})
-
-    if len(langCustom) and langCustom != "None":
-	for lang in langCustom.split(u","):
-	    lang = lang.strip()
-	    try:
-		real_lang = Language.fromietf(lang)
-	    except:
-		try:
-		    real_lang = Language.fromname(lang)
-		except:
-		    continue
-	    langList.update({real_lang})
-        
-    return langList
-
-def getSubtitleDestinationFolder():
-    if not Prefs["subtitles.save.filesystem"]:
-	return
-
-    fld_custom = Prefs["subtitles.save.subFolder.Custom"].strip() if bool(Prefs["subtitles.save.subFolder.Custom"]) else None
-    return fld_custom or (Prefs["subtitles.save.subFolder"] if Prefs["subtitles.save.subFolder"] != "current folder" else None)
 
 def initSubliminalPatches():
     # configure custom subtitle destination folders for scanning pre-existing subs
-    dest_folder = getSubtitleDestinationFolder()
+    dest_folder = config.subtitleDestinationFolder
     subliminal_patch.patch_video.CUSTOM_PATHS = [dest_folder] if dest_folder else []
     subliminal_patch.patch_provider_pool.DOWNLOAD_TRIES = int(Prefs['subtitles.try_downloads'])
     subliminal_patch.patch_providers.addic7ed.USE_BOOST = bool(Prefs['provider.addic7ed.boost'])
-
-def getProviders():
-    providers = {'opensubtitles' : Prefs['provider.opensubtitles.enabled'],
-                 'thesubdb' : Prefs['provider.thesubdb.enabled'],
-                 'podnapisi' : Prefs['provider.podnapisi.enabled'],
-                 'addic7ed' : Prefs['provider.addic7ed.enabled'],
-                 'tvsubtitles' : Prefs['provider.tvsubtitles.enabled']
-                 }
-    return filter(lambda prov: providers[prov], providers)
-
-def getProviderSettings():
-    provider_settings = {'addic7ed': {'username': Prefs['provider.addic7ed.username'], 
-                                      'password': Prefs['provider.addic7ed.password'],
-				      'use_random_agents': Prefs['provider.addic7ed.use_random_agents'],
-                                      },
-			 'opensubtitles': {'username': Prefs['provider.opensubtitles.username'], 
-                                      'password': Prefs['provider.opensubtitles.password'],
-				      },
-                        }
-
-    return provider_settings
 
 def scanTvMedia(media):
     videos = {}
@@ -130,7 +81,7 @@ def scanVideo(part, video_type):
 
 def downloadBestSubtitles(video_part_map, min_score=0):
     hearing_impaired = Prefs['subtitles.search.hearingImpaired']
-    languages = getLangList()
+    languages = config.langList
     if not languages: 
 	return
 
@@ -153,7 +104,7 @@ def downloadBestSubtitles(video_part_map, min_score=0):
     if missing_languages:
 	Log.Debug("Download best subtitles using settings: min_score: %s, hearing_impaired: %s" %(min_score, hearing_impaired))
     
-	return subliminal.api.download_best_subtitles(video_part_map.keys(), languages, min_score, hearing_impaired, providers=getProviders(), provider_configs=getProviderSettings())
+	return subliminal.api.download_best_subtitles(video_part_map.keys(), languages, min_score, hearing_impaired, providers=config.providers, provider_configs=config.providerSettings)
     Log.Debug("All languages for all requested videos exist. Doing nothing.")
 
 def saveSubtitles(videos, subtitles):
--- Contents/Code/subzero/config.py
+++ Contents/Code/subzero/config.py
@@ -1,4 +1,75 @@
+# coding=utf-8
+
+from babelfish import Language
+
 SUBTITLE_EXTS     = ['utf','utf8','utf-8','srt','smi','rt','ssa','aqt','jss','ass','idx','sub','txt', 'psb']
 VIDEO_EXTS        = ['3g2', '3gp', 'asf', 'asx', 'avc', 'avi', 'avs', 'bivx', 'bup', 'divx', 'dv', 'dvr-ms', 'evo', 'fli', 'flv',
             	 'm2t', 'm2ts', 'm2v', 'm4v', 'mkv', 'mov', 'mp4', 'mpeg', 'mpg', 'mts', 'nsv', 'nuv', 'ogm', 'ogv', 'tp',
             	 'pva', 'qt', 'rm', 'rmvb', 'sdp', 'svq3', 'strm', 'ts', 'ty', 'vdr', 'viv', 'vob', 'vp3', 'wmv', 'wpl', 'wtv', 'xsp', 'xvid', 'webm']
+
+class Config(object):
+    langList = None
+    subtitleDestinationFolder = None
+    providers = None
+    providerSettings = None
+
+    def initialize(self):
+	self.langList = self.getLangList()
+	self.subtitleDestinationFolder = self.getSubtitleDestinationFolder()
+	self.providers = self.getProviders()
+	self.providerSettings = self.getProviderSettings()
+
+    # Prepare a list of languages we want subs for
+    def getLangList(self):
+        l = {Language.fromietf(Prefs["langPref1"])}
+        langCustom = Prefs["langPrefCustom"].strip()
+
+        if Prefs["langPref2"] != "None":
+            l.update({Language.fromietf(Prefs["langPref2"])})
+
+        if Prefs["langPref3"] != "None":
+            l.update({Language.fromietf(Prefs["langPref3"])})
+
+        if len(langCustom) and langCustom != "None":
+            for lang in langCustom.split(u","):
+                lang = lang.strip()
+                try:
+                    real_lang = Language.fromietf(lang)
+                except:
+                    try:
+                        real_lang = Language.fromname(lang)
+                    except:
+                        continue
+                l.update({real_lang})
+
+        return l
+
+    def getSubtitleDestinationFolder(self):
+        if not Prefs["subtitles.save.filesystem"]:
+            return
+
+        fld_custom = Prefs["subtitles.save.subFolder.Custom"].strip() if bool(Prefs["subtitles.save.subFolder.Custom"]) else None
+        return fld_custom or (Prefs["subtitles.save.subFolder"] if Prefs["subtitles.save.subFolder"] != "current folder" else None)
+
+    def getProviders(self):
+        providers = {'opensubtitles' : Prefs['provider.opensubtitles.enabled'],
+                     'thesubdb' : Prefs['provider.thesubdb.enabled'],
+                     'podnapisi' : Prefs['provider.podnapisi.enabled'],
+                     'addic7ed' : Prefs['provider.addic7ed.enabled'],
+                     'tvsubtitles' : Prefs['provider.tvsubtitles.enabled']
+                     }
+        return filter(lambda prov: providers[prov], providers)
+
+    def getProviderSettings(self):
+        provider_settings = {'addic7ed': {'username': Prefs['provider.addic7ed.username'],
+                                          'password': Prefs['provider.addic7ed.password'],
+                                          'use_random_agents': Prefs['provider.addic7ed.use_random_agents'],
+                                          },
+                             'opensubtitles': {'username': Prefs['provider.opensubtitles.username'],
+                                          'password': Prefs['provider.opensubtitles.password'],
+                                          },
+                            }
+
+        return provider_settings
+
+config = Config()
