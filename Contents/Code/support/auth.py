ef8e0a4b13d5e230f08c7f630d8bd379215211e2
panni <panni@fragstore.net>
2015-10-28 01:56:26 +0100

    add client specific uuid to plex auth

--- Contents/Code/support/auth.py
+++ Contents/Code/support/auth.py
@@ -1,18 +1,29 @@
 # coding=utf-8
 
 import sys
+import uuid
 
 def refresh_plex_token():
     username = Prefs["plex_username"]
     password = Prefs["plex_password"]
+
     if not username or not password:
+	if "token" in Dict:
+	    del Dict["token"]
+	    Dict.Save()
 	return
+
+    if not "uuid" in Dict:
+	Dict["uuid"] = uuid.uuid1()
+	Dict.Save()
+
+    current_uuid = Dict["uuid"]
 
     headers = {
                'X-Plex-Device-Name': 'Sub-Zero',
                'X-Plex-Product': 'Sub-Zero',
                'X-Plex-Version': '1.3.0',
-               'X-Plex-Client-Identifier': "sz130",
+               'X-Plex-Client-Identifier': "%s" % current_uuid,
                }
 
     request = HTTP.Request("https://plex.tv/users/sign_in.json", headers=headers, values={'user[login]': Prefs["plex_username"], 'user[password]': Prefs["plex_password"]}, immediate=True)
