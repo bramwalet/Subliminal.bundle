b3f5bdd58db5634c9132df1a7d38456dce47f451
panni <panni@fragstore.net>
2016-05-05 04:09:48 +0200

    use locking on intents; fixes #118

--- Contents/Libraries/Shared/subzero/intent.py
+++ Contents/Libraries/Shared/subzero/intent.py
@@ -1,6 +1,9 @@
 # coding=utf-8
 
 import datetime
+import threading
+
+lock = threading.Lock()
 
 
 class TempIntent(dict):
@@ -9,7 +12,8 @@ class TempIntent(dict):
 
     def __init__(self, timeout=1000):
         self.timeout = timeout
-        self.store = {}
+        with lock:
+            self.store = {}
 
     def __getattr__(self, name):
         if name in self:
@@ -23,47 +27,51 @@ class TempIntent(dict):
             del self[name]
 
     def get(self, kind, key):
-        if kind in self["store"]:
-            now = datetime.datetime.now()
-            hit = False
-            for known_key in self["store"][kind].keys():
-                # may need locking, for now just play it safe
-                ends = self["store"][kind].get(known_key, None)
-                if not ends:
-                    continue
-
-                timed_out = False
-                if now > ends:
-                    timed_out = True
-
-                if known_key == key and not timed_out:
-                    hit = True
-
-                if timed_out:
-                    try:
-                        del self["store"][kind][key]
-                    except:
+        with lock:
+            if kind in self["store"]:
+                now = datetime.datetime.now()
+                hit = False
+                for known_key in self["store"][kind].keys():
+                    # may need locking, for now just play it safe
+                    ends = self["store"][kind].get(known_key, None)
+                    if not ends:
                         continue
 
-            if hit:
-                return True
-        return False
+                    timed_out = False
+                    if now > ends:
+                        timed_out = True
+
+                    if known_key == key and not timed_out:
+                        hit = True
+
+                    if timed_out:
+                        try:
+                            del self["store"][kind][key]
+                        except:
+                            continue
+
+                if hit:
+                    return True
+            return False
 
     def resolve(self, kind, key):
-        if kind in self["store"] and key in self["store"][kind]:
-            del self["store"][kind][key]
-            return True
-        return False
+        with lock:
+            if kind in self["store"] and key in self["store"][kind]:
+                del self["store"][kind][key]
+                return True
+            return False
 
     def set(self, kind, key, timeout=None):
-        if kind not in self["store"]:
-            self["store"][kind] = {}
-        self["store"][kind][key] = datetime.datetime.now() + datetime.timedelta(milliseconds=timeout or self.timeout)
+        with lock:
+            if kind not in self["store"]:
+                self["store"][kind] = {}
+            self["store"][kind][key] = datetime.datetime.now() + datetime.timedelta(milliseconds=timeout or self.timeout)
 
     def has(self, kind, key):
-        if kind not in self["store"]:
-            return False
-        return key in self["store"][kind]
+        with lock:
+            if kind not in self["store"]:
+                return False
+            return key in self["store"][kind]
 
 
 intent = TempIntent()
