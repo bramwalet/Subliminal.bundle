f88c7701c56136757a8d8bbb14c8298df1418afa
panni <panni@fragstore.net>
2016-12-02 12:54:00 +0100

    config: move enforce_encoding; rename rename non-SRT setting to exotic ext (SRT/ASS/SSA); exclude exotic subtitle extensions by default

--- Contents/Code/support/subtitlehelpers.py
+++ Contents/Code/support/subtitlehelpers.py
@@ -16,7 +16,7 @@ def subtitle_helpers(filename):
     filename = helpers.unicodize(filename)
     helper_classes = [DefaultSubtitleHelper]
 
-    if helpers.cast_bool(Prefs["subtitles.scan.vobsub"]):
+    if helpers.cast_bool(Prefs["subtitles.scan.exotic_ext"]):
         helper_classes.insert(0, VobSubSubtitleHelper)
 
     for cls in helper_classes:
@@ -134,12 +134,13 @@ class DefaultSubtitleHelper(SubtitleHelper):
         # IETF support thanks to https://github.com/hpsbranco/LocalMedia.bundle/commit/4fad9aefedece78a1fa96401304351347f644369
         language = Locale.Language.Match(match_ietf_language(file))
 
+        # skip non-SRT if wanted
+        if not helpers.cast_bool(Prefs["subtitles.scan.exotic_ext"]) and ext not in ["srt", "ass", "ssa"]:
+            return lang_sub_map
+
         codec = None
         format = None
         if ext in ['txt', 'sub']:
-            if not helpers.cast_bool(Prefs["subtitles.scan.vobsub"]):
-                return lang_sub_map
-
             try:
 
                 file_contents = Core.storage.load(self.filename)
