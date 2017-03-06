3e0c7e76066ec6df5c9e8b0c4ea41a32072eaf2f
pannal <panni@fragstore.net>
2017-03-06 14:59:12 +0100

    actually ditch legacy data

--- Contents/Code/support/data.py
+++ Contents/Code/support/data.py
@@ -49,8 +49,8 @@ def migrate():
                         current_key = subs["current"]
                         provider_name, subtitle_id = current_key
                         sub = subs.get(current_key)
-                        if sub:
-                            stored_subs.title = sub.get("title", "Legacy data")
+                        if sub and sub.get("title") and sub.get("mode"):  # ditch legacy data without sufficient info
+                            stored_subs.title = sub["title"]
                             new_sub = StoredSubtitle(sub["score"], sub["storage"], sub["hash"], provider_name,
                                                      subtitle_id, date_added=sub["date_added"], mode=sub["mode"])
 
