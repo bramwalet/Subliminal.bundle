8c841fec9eca445ed3930b734373007198ee4d10
pannal <panni@fragstore.net>
2017-04-20 17:11:58 +0200

    renamed patch_*.py; re-add hash validation

--- Contents/Libraries/Shared/subliminal_patch/core.py
+++ Contents/Libraries/Shared/subliminal_patch/core.py
@@ -5,13 +5,12 @@ import os
 import logging
 import socket
 import traceback
-
 import time
-
 import operator
-from collections import defaultdict
-
 import requests
+
+from collections import defaultdict
+from bs4 import UnicodeDammit
 from babelfish import LanguageReverseError
 from guessit.jsonutils import GuessitEncoder
 from subliminal import ProviderError
@@ -19,8 +18,8 @@ from subliminal import ProviderError
 from subliminal.score import compute_score as default_compute_score
 from subliminal.subtitle import SUBTITLE_EXTENSIONS, get_subtitle_path
 from subliminal.utils import hash_napiprojekt, hash_opensubtitles, hash_shooter, hash_thesubdb
-from subliminal.video import VIDEO_EXTENSIONS, Episode, Movie, Video
-from subliminal.core import guessit, Language, ProviderPool
+from subliminal.video import VIDEO_EXTENSIONS, Video
+from subliminal.core import guessit, Language, ProviderPool, io
 
 logger = logging.getLogger(__name__)
 
@@ -32,136 +31,6 @@ DOWNLOAD_TRIES = 0
 DOWNLOAD_RETRY_SLEEP = 2
 
 REMOVE_CRAP_FROM_FILENAME = re.compile("(?i)[_-](obfuscated|scrambled)(\.[\w]+)$")
-
-
-def scan_video(path, dont_use_actual_file=False, hints=None):
-    """Scan a video from a `path`.
-    
-    patch:
-        - allow passing of hints/options to guessit
-        - allow dry-run with dont_use_actual_file
-        - add crap removal (obfuscated/scrambled)
-        - trust plex's movie name
-
-    :param str path: existing path to the video.
-    :return: the scanned video.
-    :rtype: :class:`~subliminal.video.Video`
-
-    """
-    video_type = hints.get("type")
-
-    # check for non-existing path
-    if not dont_use_actual_file and not os.path.exists(path):
-        raise ValueError('Path does not exist')
-
-    # check video extension
-    if not path.endswith(VIDEO_EXTENSIONS):
-        raise ValueError('%r is not a valid video extension' % os.path.splitext(path)[1])
-
-    dirpath, filename = os.path.split(path)
-    logger.info('Scanning video %r in %r', filename, dirpath)
-
-    # hint guessit the filename itself and its 2 parent directories if we're an episode (most likely
-    # Series name/Season/filename), else only one
-    guess_from = os.path.join(*os.path.normpath(path).split(os.path.sep)[-3 if video_type == "episode" else -2:])
-    guess_from = REMOVE_CRAP_FROM_FILENAME.sub(r"\2", guess_from)
-
-    # guess
-    guessed_result = guessit(guess_from, options=hints or {})
-    logger.debug('GuessIt found: %s', json.dumps(guessed_result, cls=GuessitEncoder, indent=4, ensure_ascii=False))
-    video = Video.fromguess(path, guessed_result)
-
-    # trust plex's movie name
-    if video_type == "movie" and hints.get("expected_title"):
-        video.title = hints.get("expected_title")[0]
-
-    if dont_use_actual_file:
-        return video
-
-    # size and hashes
-    video.size = os.path.getsize(path)
-    if video.size > 10485760:
-        logger.debug('Size is %d', video.size)
-        video.hashes['opensubtitles'] = hash_opensubtitles(path)
-        video.hashes['shooter'] = hash_shooter(path)
-        video.hashes['thesubdb'] = hash_thesubdb(path)
-        video.hashes['napiprojekt'] = hash_napiprojekt(path)
-        logger.debug('Computed hashes %r', video.hashes)
-    else:
-        logger.warning('Size is lower than 10MB: hashes not computed')
-
-    return video
-
-
-def _search_external_subtitles(path, forced_tag=False):
-    dirpath, filename = os.path.split(path)
-    dirpath = dirpath or '.'
-    fileroot, fileext = os.path.splitext(filename)
-    subtitles = {}
-    for p in os.listdir(dirpath):
-        # keep only valid subtitle filenames
-        if not p.startswith(fileroot) or not p.endswith(SUBTITLE_EXTENSIONS):
-            continue
-
-        p_root, p_ext = os.path.splitext(p)
-        if not INCLUDE_EXOTIC_SUBS and p_ext not in (".srt", ".ass", ".ssa"):
-            continue
-
-        # extract potential forced/normal/default tag
-        # fixme: duplicate from subtitlehelpers
-        split_tag = p_root.rsplit('.', 1)
-        adv_tag = None
-        if len(split_tag) > 1:
-            adv_tag = split_tag[1].lower()
-            if adv_tag in ['forced', 'normal', 'default']:
-                p_root = split_tag[0]
-
-        # forced wanted but NIL
-        if forced_tag and adv_tag != "forced":
-            continue
-
-        # extract the potential language code
-        language_code = p_root[len(fileroot):].replace('_', '-')[1:]
-
-        # default language is undefined
-        language = Language('und')
-
-        # attempt to parse
-        if language_code:
-            try:
-                language = Language.fromietf(language_code)
-            except ValueError:
-                logger.error('Cannot parse language code %r', language_code)
-
-        subtitles[p] = language
-
-    logger.debug('Found subtitles %r', subtitles)
-
-    return subtitles
-
-
-def search_external_subtitles(path, forced_tag=False):
-    """
-    wrap original search_external_subtitles function to search multiple paths for one given video
-    # todo: cleanup and merge with _search_external_subtitles
-    """
-    video_path, video_filename = os.path.split(path)
-    subtitles = {}
-    for folder_or_subfolder in [video_path] + CUSTOM_PATHS:
-        # folder_or_subfolder may be a relative path or an absolute one
-        try:
-            abspath = unicode(os.path.abspath(
-                os.path.join(*[video_path if not os.path.isabs(folder_or_subfolder) else "", folder_or_subfolder,
-                               video_filename])))
-        except Exception, e:
-            logger.error("skipping path %s because of %s", repr(folder_or_subfolder), e)
-            continue
-        logger.debug("external subs: scanning path %s", abspath)
-
-        if os.path.isdir(os.path.dirname(abspath)):
-            subtitles.update(_search_external_subtitles(abspath, forced_tag=forced_tag))
-    logger.debug("external subs: found %s", subtitles)
-    return subtitles
 
 
 class PatchedProviderPool(ProviderPool):
@@ -336,6 +205,136 @@ class PatchedProviderPool(ProviderPool):
         return downloaded_subtitles
 
 
+def scan_video(path, dont_use_actual_file=False, hints=None):
+    """Scan a video from a `path`.
+
+    patch:
+        - allow passing of hints/options to guessit
+        - allow dry-run with dont_use_actual_file
+        - add crap removal (obfuscated/scrambled)
+        - trust plex's movie name
+
+    :param str path: existing path to the video.
+    :return: the scanned video.
+    :rtype: :class:`~subliminal.video.Video`
+
+    """
+    video_type = hints.get("type")
+
+    # check for non-existing path
+    if not dont_use_actual_file and not os.path.exists(path):
+        raise ValueError('Path does not exist')
+
+    # check video extension
+    if not path.endswith(VIDEO_EXTENSIONS):
+        raise ValueError('%r is not a valid video extension' % os.path.splitext(path)[1])
+
+    dirpath, filename = os.path.split(path)
+    logger.info('Scanning video %r in %r', filename, dirpath)
+
+    # hint guessit the filename itself and its 2 parent directories if we're an episode (most likely
+    # Series name/Season/filename), else only one
+    guess_from = os.path.join(*os.path.normpath(path).split(os.path.sep)[-3 if video_type == "episode" else -2:])
+    guess_from = REMOVE_CRAP_FROM_FILENAME.sub(r"\2", guess_from)
+
+    # guess
+    guessed_result = guessit(guess_from, options=hints or {})
+    logger.debug('GuessIt found: %s', json.dumps(guessed_result, cls=GuessitEncoder, indent=4, ensure_ascii=False))
+    video = Video.fromguess(path, guessed_result)
+
+    # trust plex's movie name
+    if video_type == "movie" and hints.get("expected_title"):
+        video.title = hints.get("expected_title")[0]
+
+    if dont_use_actual_file:
+        return video
+
+    # size and hashes
+    video.size = os.path.getsize(path)
+    if video.size > 10485760:
+        logger.debug('Size is %d', video.size)
+        video.hashes['opensubtitles'] = hash_opensubtitles(path)
+        video.hashes['shooter'] = hash_shooter(path)
+        video.hashes['thesubdb'] = hash_thesubdb(path)
+        video.hashes['napiprojekt'] = hash_napiprojekt(path)
+        logger.debug('Computed hashes %r', video.hashes)
+    else:
+        logger.warning('Size is lower than 10MB: hashes not computed')
+
+    return video
+
+
+def _search_external_subtitles(path, forced_tag=False):
+    dirpath, filename = os.path.split(path)
+    dirpath = dirpath or '.'
+    fileroot, fileext = os.path.splitext(filename)
+    subtitles = {}
+    for p in os.listdir(dirpath):
+        # keep only valid subtitle filenames
+        if not p.startswith(fileroot) or not p.endswith(SUBTITLE_EXTENSIONS):
+            continue
+
+        p_root, p_ext = os.path.splitext(p)
+        if not INCLUDE_EXOTIC_SUBS and p_ext not in (".srt", ".ass", ".ssa"):
+            continue
+
+        # extract potential forced/normal/default tag
+        # fixme: duplicate from subtitlehelpers
+        split_tag = p_root.rsplit('.', 1)
+        adv_tag = None
+        if len(split_tag) > 1:
+            adv_tag = split_tag[1].lower()
+            if adv_tag in ['forced', 'normal', 'default']:
+                p_root = split_tag[0]
+
+        # forced wanted but NIL
+        if forced_tag and adv_tag != "forced":
+            continue
+
+        # extract the potential language code
+        language_code = p_root[len(fileroot):].replace('_', '-')[1:]
+
+        # default language is undefined
+        language = Language('und')
+
+        # attempt to parse
+        if language_code:
+            try:
+                language = Language.fromietf(language_code)
+            except ValueError:
+                logger.error('Cannot parse language code %r', language_code)
+
+        subtitles[p] = language
+
+    logger.debug('Found subtitles %r', subtitles)
+
+    return subtitles
+
+
+def search_external_subtitles(path, forced_tag=False):
+    """
+    wrap original search_external_subtitles function to search multiple paths for one given video
+    # todo: cleanup and merge with _search_external_subtitles
+    """
+    video_path, video_filename = os.path.split(path)
+    subtitles = {}
+    for folder_or_subfolder in [video_path] + CUSTOM_PATHS:
+        # folder_or_subfolder may be a relative path or an absolute one
+        try:
+            abspath = unicode(os.path.abspath(
+                os.path.join(*[video_path if not os.path.isabs(folder_or_subfolder) else "", folder_or_subfolder,
+                               video_filename])))
+        except Exception, e:
+            logger.error("skipping path %s because of %s", repr(folder_or_subfolder), e)
+            continue
+        logger.debug("external subs: scanning path %s", abspath)
+
+        if os.path.isdir(os.path.dirname(abspath)):
+            subtitles.update(_search_external_subtitles(abspath, forced_tag=forced_tag))
+    logger.debug("external subs: found %s", subtitles)
+    return subtitles
+
+
 def list_all_subtitles(videos, languages, **kwargs):
     """List all available subtitles.
     
@@ -368,3 +367,111 @@ def list_all_subtitles(videos, languages, **kwargs):
             logger.info('Found %d subtitle(s)', len(subtitles))
 
     return listed_subtitles
+
+
+def get_subtitle_path(video_path, language=None, extension='.srt', forced_tag=False):
+    """Get the subtitle path using the `video_path` and `language`.
+
+    :param str video_path: path to the video.
+    :param language: language of the subtitle to put in the path.
+    :type language: :class:`~babelfish.language.Language`
+    :param str extension: extension of the subtitle.
+    :return: path of the subtitle.
+    :rtype: str
+
+    """
+    subtitle_root = os.path.splitext(video_path)[0]
+
+    if language:
+        subtitle_root += '.' + str(language)
+
+    if forced_tag:
+        subtitle_root += ".forced"
+
+    return subtitle_root + extension
+
+
+def save_subtitles(video, subtitles, single=False, directory=None, encoding=None, encode_with=None, chmod=None,
+                   forced_tag=False, path_decoder=None):
+    """Save subtitles on filesystem.
+
+    Subtitles are saved in the order of the list. If a subtitle with a language has already been saved, other subtitles
+    with the same language are silently ignored.
+
+    The extension used is `.lang.srt` by default or `.srt` is `single` is `True`, with `lang` being the IETF code for
+    the :attr:`~subliminal.subtitle.Subtitle.language` of the subtitle.
+
+    :param video: video of the subtitles.
+    :type video: :class:`~subliminal.video.Video`
+    :param subtitles: subtitles to save.
+    :type subtitles: list of :class:`~subliminal.subtitle.Subtitle`
+    :param bool single: save a single subtitle, default is to save one subtitle per language.
+    :param str directory: path to directory where to save the subtitles, default is next to the video.
+    :param str encoding: encoding in which to save the subtitles, default is to keep original encoding.
+    :return: the saved subtitles
+    :rtype: list of :class:`~subliminal.subtitle.Subtitle`
+
+    patch: unicode path problems
+    """
+    saved_subtitles = []
+    for subtitle in subtitles:
+        # check content
+        if subtitle.content is None:
+            logger.error('Skipping subtitle %r: no content', subtitle)
+            continue
+
+        # check language
+        if subtitle.language in set(s.language for s in saved_subtitles):
+            logger.debug('Skipping subtitle %r: language already saved', subtitle)
+            continue
+
+        # create subtitle path
+        subtitle_path = get_subtitle_path(video.name, None if single else subtitle.language, forced_tag=forced_tag)
+        if directory is not None:
+            subtitle_path = os.path.join(directory, os.path.split(subtitle_path)[1])
+
+        if path_decoder:
+            subtitle_path = path_decoder(subtitle_path)
+
+        # force unicode
+        subtitle_path = UnicodeDammit(subtitle_path).unicode_markup
+
+        subtitle.storage_path = subtitle_path
+
+        # save content as is or in the specified encoding
+        logger.info('Saving %r to %r', subtitle, subtitle_path)
+        has_encoder = callable(encode_with)
+
+        if has_encoder:
+            logger.info('Using encoder %s' % encode_with.__name__)
+
+        # save normalized subtitle if encoder or no encoding is given
+        if has_encoder or encoding is None:
+            content = encode_with(subtitle.text) if has_encoder else subtitle.content
+            with io.open(subtitle_path, 'wb') as f:
+                f.write(content)
+
+            # change chmod if requested
+            if chmod:
+                os.chmod(subtitle_path, chmod)
+
+            if single:
+                break
+            continue
+
+        # save subtitle if encoding given
+        if encoding is not None:
+            with io.open(subtitle_path, 'w', encoding=encoding) as f:
+                f.write(subtitle.text)
+
+        # change chmod if requested
+        if chmod:
+            os.chmod(subtitle_path, chmod)
+
+        saved_subtitles.append(subtitle)
+
+        # check single
+        if single:
+            break
+
+    return saved_subtitles
--- Contents/Libraries/Shared/subliminal_patch/score.py
+++ Contents/Libraries/Shared/subliminal_patch/score.py
@@ -8,7 +8,7 @@ from subliminal.score import get_scores
 logger = logging.getLogger(__name__)
 
 
-def compute_score(matches, subtitle, video, hearing_impaired=None):
+def compute_score(orig_matches, subtitle, video, hearing_impaired=None):
     """Compute the score of the `subtitle` against the `video` with `hearing_impaired` preference.
     
     patch: 
@@ -28,18 +28,35 @@ def compute_score(matches, subtitle, video, hearing_impaired=None):
 
     """
     logger.info('Computing score of %r for video %r with %r', subtitle, video, dict(hearing_impaired=hearing_impaired))
+    matches = orig_matches.copy()
 
     # get the scores dict
     scores = get_scores(video)
     logger.debug('Using scores %r', scores)
 
+    is_episode = isinstance(video, Episode)
+    is_movie = isinstance(video, Movie)
+
+    episode_hash_valid_if = {"series", "season", "episode", "format"}
+    movie_hash_valid_if = {"video_codec", "format"}
+
     # on hash match, discard everything else
     if 'hash' in matches:
-        logger.debug('Keeping only hash match')
-        matches &= {'hash'}
+        # hash is error-prone, try to fix that
+        hash_valid_if = episode_hash_valid_if if is_episode else movie_hash_valid_if
+
+        if hash_valid_if <= set(matches):
+            # series, season and episode matched, hash is valid
+            logger.debug('Using valid hash, as %s are correct (%r) and (%r)', hash_valid_if, matches, video)
+            matches &= {'hash', 'hearing_impaired'}
+        else:
+            # no match, invalidate hash
+            logger.debug('Ignoring hash as other matches are wrong (missing: %r) and (%r)', hash_valid_if - matches,
+                         video)
+            matches -= {"hash"}
 
     # handle equivalent matches
-    if isinstance(video, Episode):
+    if is_episode:
         if 'title' in matches:
             logger.debug('Adding title match equivalent')
             matches.add('episode')
@@ -55,7 +72,7 @@ def compute_score(matches, subtitle, video, hearing_impaired=None):
         if 'series_tvdb_id' in matches:
             logger.debug('Adding series_tvdb_id match equivalents')
             matches |= {'series', 'year'}
-    elif isinstance(video, Movie):
+    elif is_movie:
         if 'imdb_id' in matches:
             logger.debug('Adding imdb_id match equivalents')
             matches |= {'title', 'year'}
@@ -68,8 +85,5 @@ def compute_score(matches, subtitle, video, hearing_impaired=None):
     # compute the score
     score = sum((scores.get(match, 0) for match in matches))
     logger.info('Computed score %r with final matches %r', score, matches)
-
-    # ensure score is within valid bounds
-    assert 0 <= score
 
     return score
