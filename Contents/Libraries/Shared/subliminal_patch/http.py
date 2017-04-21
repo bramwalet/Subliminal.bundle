ac4153b58a66c7c370bee21962f9df80db5c77b1
pannal <panni@fragstore.net>
2017-04-22 01:13:26 +0200

    add and use certifi

--- Contents/Libraries/Shared/subliminal_patch/http.py
+++ Contents/Libraries/Shared/subliminal_patch/http.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-import os
+import certifi
 
 from requests import Session
 from retry.api import retry_call
@@ -11,7 +11,8 @@ class RetryingSession(Session):
 
     def __init__(self):
         super(RetryingSession, self).__init__()
-        self.verify = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "certs.pem"))
+        self.verify = certifi.where()
+        print self.verify
 
     def retry_method(self, method, *args, **kwargs):
         return retry_call(getattr(super(RetryingSession, self), method), fargs=args, fkwargs=kwargs, tries=3, delay=1)
