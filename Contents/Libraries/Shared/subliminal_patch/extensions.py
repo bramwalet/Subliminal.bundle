98de57f9ad59b843bd41b94fcbbeec5d2b037c2c
pannal <panni@fragstore.net>
2017-04-21 23:51:07 +0200

    re-add opensubtitles

--- Contents/Libraries/Shared/subliminal_patch/extensions.py
+++ Contents/Libraries/Shared/subliminal_patch/extensions.py
@@ -4,7 +4,7 @@ from subliminal.extensions import RegistrableExtensionManager
 provider_manager = RegistrableExtensionManager('subliminal.providers', [
     'addic7ed = subliminal_patch.providers.addic7ed:PatchedAddic7edProvider',
     'legendastv = subliminal.providers.legendastv:LegendasTVProvider',
-    'opensubtitles = subliminal.providers.opensubtitles:OpenSubtitlesProvider',
+    'opensubtitles = subliminal_patch.providers.opensubtitles:PatchedOpenSubtitlesProvider',
     'podnapisi = subliminal.providers.podnapisi:PodnapisiProvider',
     'shooter = subliminal.providers.shooter:ShooterProvider',
     'subscenter = subliminal.providers.subscenter:SubsCenterProvider',
