a0bc73ab3b487d174c843c268f7aba42924d5547
pannal <panni@fragstore.net>
2017-04-22 01:06:46 +0200

    update provider_test.sh; add certs.pem; use certs.pem with requests.Session

--- Contents/Libraries/Shared/subliminal_patch/http.py
+++ Contents/Libraries/Shared/subliminal_patch/http.py
@@ -1,11 +1,17 @@
 # coding=utf-8
 
+import os
+
 from requests import Session
 from retry.api import retry_call
 
 
 class RetryingSession(Session):
     proxied_functions = ("get", "post")
+
+    def __init__(self):
+        super(RetryingSession, self).__init__()
+        self.verify = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "certs.pem"))
 
     def retry_method(self, method, *args, **kwargs):
         return retry_call(getattr(super(RetryingSession, self), method), fargs=args, fkwargs=kwargs, tries=3, delay=1)
