179df2fbcc77effe2597a95b16aae560a478d048
pannal <panni@fragstore.net>
2017-04-22 03:24:53 +0200

    set addicted default boost to 25

--- Contents/Code/support/config.py
+++ Contents/Code/support/config.py
@@ -388,7 +388,7 @@ class Config(object):
         subliminal_patch.core.INCLUDE_EXOTIC_SUBS = self.exotic_ext
 
         subliminal_patch.core.DOWNLOAD_TRIES = int(Prefs['subtitles.try_downloads'])
-        subliminal.score.episode_scores["addic7ed_boost"] = int(Prefs['provider.addic7ed.boost_by'])
+        subliminal.score.episode_scores["addic7ed_boost"] = int(Prefs['provider.addic7ed.boost_by1'])
 
 
 config = Config()
