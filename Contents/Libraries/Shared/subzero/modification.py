78191bb750970a95cd63579070f66b8efde029d2
pannal <panni@fragstore.net>
2017-04-29 22:51:18 +0200

    doc; all caps line replaced with at least 3 chars; accept filename or subtitle content; optional known fps argument

--- Contents/Libraries/Shared/subzero/modification.py
+++ Contents/Libraries/Shared/subzero/modification.py
@@ -1,18 +1,37 @@
 # coding=utf-8
 
 import re
-import pysubs2
+import traceback
 
-characters_considered_empty_when_alone = r"[\s.,-_!?]"
+import pysubs2
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class SubtitleModifications(object):
-    def __init__(self, fn):
-        self.f = pysubs2.load("test.srt")
+    def __init__(self, fn, content, fps=None):
+        if fn:
+            try:
+                self.f = pysubs2.load(fn, fps=fps)
+            except (IOError,
+                    UnicodeDecodeError,
+                    pysubs2.exceptions.UnknownFPSError,
+                    pysubs2.exceptions.UnknownFormatIdentifierError,
+                    pysubs2.exceptions.FormatAutodetectionError):
+                logger.exception("Couldn't load subtitle: %s: %s", fn, traceback.format_exc())
+        elif content:
+            try:
+                self.f = pysubs2.SSAFile.from_string(content, fps=fps)
+            except (UnicodeDecodeError,
+                    pysubs2.exceptions.UnknownFPSError,
+                    pysubs2.exceptions.UnknownFormatIdentifierError,
+                    pysubs2.exceptions.FormatAutodetectionError):
+                logger.exception("Couldn't load subtitle: %s", traceback.format_exc())
 
     def modify(self, *mods):
         new_f = []
-        for key, line in enumerate(self.f):
+        for line in self.f:
             for mod in mods:
                 new_content = mod.modify(line.text)
                 if not new_content:
@@ -27,15 +46,24 @@ class SubtitleModifications(object):
     def save(self, fn):
         self.f.save(fn)
 
+
 SubMod = SubtitleModifications
 
 
 class Processor(object):
+    """
+    Processor base class
+    """
+
     def process(self, content):
         return content
 
 
 class StringProcessor(Processor):
+    """
+    String replacement processor base
+    """
+
     def __init__(self, search, replace):
         self.search = search
         self.replace = replace
@@ -45,6 +73,9 @@ class StringProcessor(Processor):
 
 
 class ReProcessor(Processor):
+    """
+    Regex processor
+    """
     pattern = None
     replace_with = None
 
@@ -57,6 +88,10 @@ class ReProcessor(Processor):
 
 
 class NReProcessor(ReProcessor):
+    """
+    Single line regex processor
+    """
+
     def process(self, content):
         lines = []
         for line in content.split(r"\N"):
@@ -106,7 +141,7 @@ class SubtitleModification(object):
 class SubtitleTextModification(SubtitleModification):
     post_processors = [
         # empty tag
-        ReProcessor(r'(<[A-z]+[^>]*>)%s+(</[A-z]>)' % characters_considered_empty_when_alone, ""),
+        ReProcessor(r'(<[A-z]+[^>]*>)[\s.,-_!?]+(</[A-z]>)', ""),
 
         # empty line (needed?)
         ReProcessor(r'(?m)^\s+$', ""),
@@ -114,7 +149,7 @@ class SubtitleTextModification(SubtitleModification):
         # empty dash line (needed?)
         ReProcessor(r'(?m)(^[\s]*[\-]+[\s]*)$', ""),
 
-        # clean start or end
+        # clean whitespace at start and end
         ReProcessor(r'^\s*([^\s]+)\s*$', r"\1"),
     ]
 
@@ -124,10 +159,9 @@ class HearingImpaired(SubtitleTextModification):
         # brackets
         ReProcessor(r'(?sux)[([{].+[)\]}]', ""),
 
-        # text before colon
+        # text before colon (and possible dash in front)
         ReProcessor(r'(?mu)(^[A-z\-]+[\w\s]*:[^0-9{2}][\s]*)', ""),
 
-        # all caps line
-        NReProcessor(r'(?mu)(^[A-Z\.\-_]+$)', ""),
+        # all caps line (at least 3 chars
+        NReProcessor(r'(?mu)(^[A-Z\.\-_]{3,}$)', ""),
     ]
-
