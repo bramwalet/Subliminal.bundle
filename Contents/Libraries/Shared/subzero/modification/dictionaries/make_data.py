6f829dd4c7cecd1cb620695aac38b13179ab3fe3
panni <panni@fragstore.net>
2017-05-02 18:43:35 +0200

    move xmls to xml/; add make_data and test_data script;

---
+++ Contents/Libraries/Shared/subzero/modification/dictionaries/make_data.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+
+import os
+import pprint
+from collections import OrderedDict
+
+from bs4 import BeautifulSoup
+
+TEMPLATE = """\
+from collections import OrderedDict
+data = """
+
+
+if __name__ == "__main__":
+    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    xml_dir = os.path.join(cur_dir, "xml")
+    file_list = os.listdir(xml_dir)
+
+    data = {}
+
+    for fn in file_list:
+        if fn.endswith("_OCRFixReplaceList.xml"):
+            lang = fn.split("_")[0]
+            soup = BeautifulSoup(open(os.path.join(xml_dir, fn)), "xml")
+
+            data[lang] = OrderedDict()
+
+            for wanted in ("WholeLines", "WholeWords", "PartialWordsAlways"):
+                for grp in soup.find_all(wanted):
+                    for line in grp.find_all(["Line", "Word", "WordPart"]):
+                        data[lang][line["from"]] = line["to"]
+
+    f = open(os.path.join(cur_dir, "data.py"), "w+")
+    f.write(TEMPLATE)
+    f.write(pprint.pformat(data, width=1))
+    f.close()
---
+++ Contents/Libraries/Shared/subzero/modification/dictionaries/test_data.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+
+from data import data
+
+for find, rep in data["dan"].iteritems():
+    print find, rep
