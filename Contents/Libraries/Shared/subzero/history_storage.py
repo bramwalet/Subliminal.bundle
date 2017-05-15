cb94f0c2c6445981933350927d7a2bb78439f98f
panni <panni@fragstore.net>
2017-05-15 18:09:31 +0200

    remove invalid comment

--- Contents/Libraries/Shared/subzero/history_storage.py
+++ Contents/Libraries/Shared/subzero/history_storage.py
@@ -76,7 +76,6 @@ class SubtitleHistory(object):
             self.history_items = []
 
     def add(self, item_title, rating_key, section_title=None, subtitle=None, mode="a", time=None):
-        # create copy
         items = self.history_items
         item = SubtitleHistoryItem(item_title, rating_key, section_title=section_title, subtitle=subtitle, mode=mode, time=time)
 
