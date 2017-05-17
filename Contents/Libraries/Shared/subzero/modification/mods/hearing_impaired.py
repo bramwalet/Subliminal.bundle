fb494a911d0e484a82eae07db6e1d099202b8750
panni <panni@fragstore.net>
2017-05-17 20:15:45 +0200

    fix character ranges

--- Contents/Libraries/Shared/subzero/modification/mods/hearing_impaired.py
+++ Contents/Libraries/Shared/subzero/modification/mods/hearing_impaired.py
@@ -17,16 +17,16 @@ class HearingImpaired(SubtitleTextModification):
 
     processors = [
         # brackets (only remove if at least 3 consecutive uppercase chars in brackets
-        NReProcessor(re.compile(r'(?sux)[([].+(?=[À-Ž]{3,}).+[)\]]'), "", name="HI_brackets"),
+        NReProcessor(re.compile(ur'(?sux)[([].+(?=[A-ZÀ-Ž]{3,}).+[)\]]'), "", name="HI_brackets"),
 
         # text before colon (and possible dash in front), max 11 chars after the first whitespace (if any)
         #NReProcessor(re.compile(r'(?u)(^[A-z\-\'"_]+[\w\s]{0,11}:[^0-9{2}][\s]*)'), "", name="HI_before_colon"),
 
         # text before colon (at least 4 consecutive uppercase chars)
-        NReProcessor(re.compile(r'(?u)(^(?=.*[À-Ž]{4,})[À-Ž-_\s]+:[^0-9]\s*)'), "", name="HI_before_colon"),
+        NReProcessor(re.compile(ur'(?u)(^(?=.*[A-ZÀ-Ž]{4,})[A-ZÀ-Ž-_\s]+:[^0-9]\s*)'), "", name="HI_before_colon"),
 
         # all caps line (at least 4 consecutive uppercase chars)
-        NReProcessor(re.compile(r'(?u)(^(?=.*[À-Ž]{4,})[À-Ž-_\s]+$)'), "", name="HI_all_caps"),
+        NReProcessor(re.compile(ur'(?u)(^(?=.*[A-ZÀ-Ž]{4,})[A-ZÀ-Ž-_\s]+$)'), "", name="HI_all_caps"),
 
         # dash in front
         # NReProcessor(re.compile(r'(?u)^\s*-\s*'), "", name="HI_starting_dash"),
