e167439ed03a79ee1536e3bf9ecbde27433ff493
panni <panni@fragstore.net>
2017-05-27 03:09:50 +0200

    add lang_list to config debug

--- Contents/Code/interface/menu.py
+++ Contents/Code/interface/menu.py
@@ -201,7 +201,7 @@ def ValidatePrefs():
     for attr in [
             "app_support_path", "data_path", "data_items_path", "enable_agent",
             "enable_channel", "permissions_ok", "missing_permissions", "fs_encoding", "enforce_encoding",
-            "subtitle_destination_folder", "dbm_supported"]:
+            "subtitle_destination_folder", "dbm_supported", "lang_list"]:
         Log.Debug("config.%s: %s", attr, getattr(config, attr))
 
     for attr in ["plugin_log_path", "server_log_path"]:
