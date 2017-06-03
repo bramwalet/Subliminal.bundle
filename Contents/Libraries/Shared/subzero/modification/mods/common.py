93517582d1457b81c20c97d0b14720e753f42daf
panni <panni@fragstore.net>
2017-06-03 23:18:54 +0200

    submod: common: be smarter about spaces in numbers

--- Contents/Libraries/Shared/subzero/modification/mods/common.py
+++ Contents/Libraries/Shared/subzero/modification/mods/common.py
@@ -53,13 +53,16 @@ class CommonFixes(SubtitleTextModification):
 
         # replace uppercase I with lowercase L in words
         NReProcessor(re.compile(ur'(?u)([A-zÀ-ž][a-zà-ž]+)(I+)'),
-                     lambda match: ur'%s%s' % (match.group(1), "l"*len(match.group(2))), name="CM_uppercase_i_in_word"),
+                     lambda match: ur'%s%s' % (match.group(1), "l" * len(match.group(2))),
+                     name="CM_uppercase_i_in_word"),
 
-        # fix spaces in numbers (allows for punctuation: ,.:' (comma only fixed if after space, those may be
+        # fix spaces in numbers (allows for punctuation: ,.:' (comma/dot only fixed if after space, those may be
         # countdowns otherwise); don't break up ellipses
         # fixme: maybe check whether it's a countdown (second part smaller than the first), otherwise handle default?
-        NReProcessor(re.compile(r'(?u)([0-9]+[0-9.:\']*(?<!\.\.))\s+((?!\.\.)[0-9,.:\']*[0-9]+)'), r"\1\2",
-                     name="CM_spaces_in_numbers"),
+        NReProcessor(
+            re.compile(r'(?u)([0-9]+[0-9:\']*(?<!\.\.)\s+(?!\.\.)[0-9,.:\']*(?=[0-9]+)[0-9,.:\'\s]+)(?=\s|$)'),
+            lambda match: match.group(1).replace(" ", ""),
+            name="CM_spaces_in_numbers"),
 
         # uppercase after dot
         NReProcessor(re.compile(ur'(?u)((?:[^.\s])+\.\s+)([a-zà-ž])'),
