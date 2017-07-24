11d5edcc5e5c5541093b05d264364766fc136f9c
panni <panni@fragstore.net>
2017-07-24 14:38:02 +0200

    legendastv: don't permanently store subtitle.archive.content, because of picklability

--- Contents/Libraries/Shared/subliminal_patch/providers/legendastv.py
+++ Contents/Libraries/Shared/subliminal_patch/providers/legendastv.py
@@ -10,6 +10,7 @@ logger = logging.getLogger(__name__)
 class LegendasTVSubtitle(_LegendasTVSubtitle):
     def __init__(self, language, type, title, year, imdb_id, season, archive, name):
         super(LegendasTVSubtitle, self).__init__(language, type, title, year, imdb_id, season, archive, name)
+        self.archive.content = None
         self.release_info = archive.name
         self.page_link = archive.link
 
@@ -20,3 +21,7 @@ class LegendasTVSubtitle(_LegendasTVSubtitle):
 
 class LegendasTVProvider(_LegendasTVProvider):
     subtitle_class = LegendasTVSubtitle
+
+    def download_subtitle(self, subtitle):
+        super(LegendasTVProvider, self).download_subtitle(subtitle)
+        subtitle.archive.content = None
