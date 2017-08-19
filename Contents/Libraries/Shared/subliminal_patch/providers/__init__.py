5fc1c8cbb1dc37482cab9ffcc0a075fd0122ec8a
panni <panni@fragstore.net>
2017-08-19 07:06:47 +0200

    "fix" provider_registry

--- Contents/Libraries/Shared/subliminal_patch/providers/__init__.py
+++ Contents/Libraries/Shared/subliminal_patch/providers/__init__.py
@@ -2,6 +2,7 @@
 
 import importlib
 import os
+import subliminal
 from subliminal.providers import Provider as _Provider
 from subliminal.subtitle import Subtitle as _Subtitle
 from subliminal_patch.extensions import provider_registry
@@ -18,6 +19,7 @@ class Provider(_Provider):
 
 
 # register providers
+# fixme: this is bad
 for name in os.listdir(os.path.dirname(unicode(__file__, get_viable_encoding()))):
     if name in ("__init__.py", "mixins.py", "utils.py") or not name.endswith(".py"):
         continue
@@ -25,34 +27,45 @@ for name in os.listdir(os.path.dirname(unicode(__file__, get_viable_encoding()))
     module_name = os.path.splitext(name)[0]
     mod = importlib.import_module("subliminal_patch.providers.%s" % module_name.lower())
     for item in dir(mod):
-        if item.endswith("Provider") and not item.startswith("_"):
-            provider_class = getattr(mod, item)
-            is_sz_provider = issubclass(provider_class, Provider)
+        cls = getattr(mod, item)
+        if item != "Provider" and item.endswith("Provider") and not item.startswith("_"):
+            is_sz_provider = issubclass(cls, Provider)
+            is_provider = issubclass(cls, _Provider)
+
+            if not is_provider:
+                continue
 
             if not is_sz_provider:
                 # patch provider bases
                 new_bases = []
-                for base in provider_class.__bases__:
-                    if issubclass(base, _Provider):
-                        base.__bases__ = (Provider,)
+
+                for base in cls.__bases__:
+                    if base == _Provider:
+                        base = Provider
+                    else:
+                        if _Provider in base.__bases__:
+                            base.__bases__ = (Provider,)
                     new_bases.append(base)
 
-                provider_class.__bases__ = tuple(new_bases)
+                cls.__bases__ = tuple(new_bases)
 
                 # patch subtitle bases
                 new_bases = []
-                for base in provider_class.subtitle_class.__bases__:
-                    if issubclass(base, _Subtitle):
-                        base.__bases__ = (Subtitle,)
+                for base in cls.subtitle_class.__bases__:
+                    if base == _Subtitle:
+                        base = Subtitle
+                    else:
+                        if _Subtitle in base.__bases__:
+                            base.__bases__ = (Subtitle,)
                     new_bases.append(base)
 
-                provider_class.subtitle_class.__bases__ = tuple(new_bases)
+                cls.subtitle_class.__bases__ = tuple(new_bases)
 
             # inject our requests.Session wrapper for automatic retry
             mod.Session = RetryingSession
             mod.guess_matches = guess_matches
 
-            provider_registry.register(module_name, provider_class)
+            provider_registry.register(module_name, cls)
 
     # try patching the correspondent subliminal provider
     try:
