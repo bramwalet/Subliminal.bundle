eb1c5d976f9644cd77b293832d3478407e4653ee
panni <panni@fragstore.net>
2017-10-30 17:36:10 +0100

    #339 also ignore country part in existing subs; possible fix

--- Contents/Code/support/download.py
+++ Contents/Code/support/download.py
@@ -10,12 +10,13 @@ from subliminal_patch import compute_score
 
 def download_best_subtitles(video_part_map, min_score=0):
     hearing_impaired = Prefs['subtitles.search.hearingImpaired']
+    ietf_as_alpha3 = cast_bool(Prefs["subtitles.language.ietf"])
     languages = config.lang_list.copy()
     if not languages:
         return
 
     # should we treat IETF as alpha3? (ditch the country part)
-    if cast_bool(Prefs["subtitles.language.ietf"]):
+    if ietf_as_alpha3:
         for language in languages:
             language.country_orig = language.country
             language.country = None
@@ -30,7 +31,13 @@ def download_best_subtitles(video_part_map, min_score=0):
                     video.subtitle_languages.add(language)
                     Log.Debug("Found metadata subtitle %s for %s", language, video)
 
-        missing_subs = (languages - video.subtitle_languages)
+        have_languages = video.subtitle_languages.copy()
+        if ietf_as_alpha3:
+            for language in have_languages:
+                language.country_orig = language.country
+                language.country = None
+
+        missing_subs = (languages - have_languages)
 
         # all languages are found if we either really have subs for all languages or we only want to have exactly one language
         # and we've only found one (the case for a selected language, Prefs['subtitles.only_one'] (one found sub matches any language))
@@ -46,7 +53,7 @@ def download_best_subtitles(video_part_map, min_score=0):
 
     if missing_languages:
         # re-add country codes to the missing languages, in case we've removed them above
-        if cast_bool(Prefs["subtitles.language.ietf"]):
+        if ietf_as_alpha3:
             for language in languages:
                 if language.country_orig:
                     language.country = language.country_orig
