a869281de72ddeebf5568a0d57daf0f0dc0e0ee5
panni <panni@fragstore.net>
2018-02-02 22:43:01 +0100

    core: add missing custom Video attributes

--- Contents/Libraries/Shared/subliminal_patch/video.py
+++ Contents/Libraries/Shared/subliminal_patch/video.py
@@ -8,6 +8,9 @@ from subliminal.video import Video as Video_
 class Video(Video_):
     is_special = False
     fps = None
+    plexapi_metadata = None
+    hints = None
+    season_fully_aired = None
 
     def __init__(self, name, format=None, release_group=None, resolution=None, video_codec=None, audio_codec=None,
                  imdb_id=None, hashes=None, size=None, subtitle_languages=None):
@@ -15,3 +18,5 @@ class Video(Video_):
                                     video_codec=video_codec, audio_codec=audio_codec, imdb_id=imdb_id, hashes=hashes,
                                     size=size, subtitle_languages=subtitle_languages)
         self.original_name = os.path.basename(name)
+        self.plexapi_metadata = {}
+        self.hints = {}
