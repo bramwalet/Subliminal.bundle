076ad783554a21918edb9c3957e2f3329334f998
panni <panni@fragstore.net>
2018-03-17 01:55:15 +0100

    remove comment

--- Contents/Code/support/scanning.py
+++ Contents/Code/support/scanning.py
@@ -81,11 +81,6 @@ def scan_video(pms_video_info, ignore_all=False, hints=None, rating_key=None, pr
 
     try:
         # get basic video info scan (filename)
-        # video = parse_video(plex_part.file, pms_video_info, hints, external_subtitles=external_subtitles,
-        #                     embedded_subtitles=embedded_subtitles, known_embedded=known_embedded,
-        #                     forced_only=config.forced_only, no_refining=no_refining, ignore_all=ignore_all,
-        #                     stored_subs=stored_subs, refiner_settings=config.refiner_settings, providers=providers,
-        #                     skip_hashing=config.low_impact_mode)
         video = parse_video(plex_part.file, hints, skip_hashing=config.low_impact_mode or skip_hashing,
                             providers=providers)
 
