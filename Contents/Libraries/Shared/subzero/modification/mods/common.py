d936460d83f7abc6f28b89c471f0d2441e1b64fd
panni <panni@fragstore.net>
2018-03-27 17:13:05 +0200

    submod: common: extend non_word_only matching

--- Contents/Libraries/Shared/subzero/modification/mods/common.py
+++ Contents/Libraries/Shared/subzero/modification/mods/common.py
@@ -24,7 +24,7 @@ class CommonFixes(SubtitleTextModification):
         NReProcessor(re.compile(r'(?u)(\w|\b|\s|^)(-\s?-{1,2})'), ur"\1—", name="CM_multidash"),
 
         # line = _/-/\s
-        NReProcessor(re.compile(r'(?u)(^[-_\s.]*[-_\s.]+[-_\s.]*$)'), "", name="CM_non_word_only"),
+        NReProcessor(re.compile(r'(?u)(^\W*[-_.]+\W*$)'), "", name="CM_non_word_only"),
 
         # fix music symbols
         NReProcessor(re.compile(ur'(?u)(^[*#¶\s]*[*#¶]+[*#¶\s]*$)'), u"♪", name="CM_music_symbols"),
