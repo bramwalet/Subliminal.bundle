20a0993aa86bbe5791c096b4c8856fddba95eb5a
panni <panni@fragstore.net>
2018-04-06 14:10:40 +0200

    tasks: move provider slack vars to class

--- Contents/Code/interface/menu_helpers.py
+++ Contents/Code/interface/menu_helpers.py
@@ -139,6 +139,7 @@ def debounce(func):
                 Log.Debug("not triggering %s twice with %s, %s" % (func, args, kwargs))
                 return ObjectContainer()
             else:
+                # fixme: set limit
                 Dict["menu_history"][key] = datetime.datetime.now() + datetime.timedelta(hours=6)
                 try:
                     Dict.Save()
