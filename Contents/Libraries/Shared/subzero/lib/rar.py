4b7664aaa6e93c4e85c1b66522cd25ec777427b9
panni <panni@fragstore.net>
2018-04-06 17:44:41 +0200

    use rarfile explicitly instead of individual imports

--- Contents/Libraries/Shared/subzero/lib/rar.py
+++ Contents/Libraries/Shared/subzero/lib/rar.py
@@ -1,14 +1,12 @@
 # coding=utf-8
 
 import logging
-
-from rarfile import RarFile as _RarFile, UNRAR_TOOL, ORIG_OPEN_ARGS as OPEN_ARGS, custom_popen, check_returncode, \
-    XTempFile
+import rarfile
 
 log = logging.getLogger(__name__)
 
 
-class RarFile(_RarFile):
+class RarFile(rarfile.RarFile):
     def read(self, fname, psw=None):
         """
         read specific content of rarfile without parsing
@@ -16,12 +14,12 @@ class RarFile(_RarFile):
         :param psw:
         :return:
         """
-        cmd = [UNRAR_TOOL] + list(OPEN_ARGS)
+        cmd = [rarfile.UNRAR_TOOL] + list(rarfile.OPEN_ARGS)
 
-        with XTempFile(self._rarfile) as rf:
+        with rarfile.XTempFile(self._rarfile) as rf:
             log.debug(u"RAR CMD: %s", cmd + [rf, fname])
-            p = custom_popen(cmd + [rf, fname])
+            p = rarfile.custom_popen(cmd + [rf, fname])
             output = p.communicate()[0]
-            check_returncode(p, output)
+            rarfile.check_returncode(p, output)
 
             return output
