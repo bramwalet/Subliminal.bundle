390af30bf65ba9f6612ad3848aef0d72e337a628
panni <panni@fragstore.net>
2018-04-06 17:22:49 +0200

    add cmd debug to rar; set rar.UNRAR_TOOL

--- Contents/Libraries/Shared/subzero/lib/rar.py
+++ Contents/Libraries/Shared/subzero/lib/rar.py
@@ -1,8 +1,11 @@
 # coding=utf-8
 
+import logging
 
-from rarfile import RarFile as _RarFile, UNRAR_TOOL, ORIG_OPEN_ARGS as OPEN_ARGS, custom_popen, check_returncode,\
+from rarfile import RarFile as _RarFile, UNRAR_TOOL, ORIG_OPEN_ARGS as OPEN_ARGS, custom_popen, check_returncode, \
     XTempFile
+
+log = logging.getLogger(__name__)
 
 
 class RarFile(_RarFile):
@@ -16,6 +19,7 @@ class RarFile(_RarFile):
         cmd = [UNRAR_TOOL] + list(OPEN_ARGS)
 
         with XTempFile(self._rarfile) as rf:
+            log.debug(u"RAR CMD: %s", cmd + [rf, fname])
             p = custom_popen(cmd + [rf, fname])
             output = p.communicate()[0]
             check_returncode(p, output)
