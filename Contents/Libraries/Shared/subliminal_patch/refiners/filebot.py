42b7e9fa62cb883177b9aac0a485c26c77634948
panni <panni@fragstore.net>
2018-05-25 16:34:02 +0200

    add logging for the filebot refiner

--- Contents/Libraries/Shared/subliminal_patch/refiners/filebot.py
+++ Contents/Libraries/Shared/subliminal_patch/refiners/filebot.py
@@ -1,7 +1,10 @@
 # coding=utf-8
 
+import logging
 from libfilebot import get_filebot_attrs
 from common import update_video
+
+logger = logging.getLogger(__name__)
 
 
 def refine(video, **kwargs):
@@ -15,3 +18,5 @@ def refine(video, **kwargs):
 
     if orig_fn:
         update_video(video, orig_fn)
+    else:
+        logger.info(u"%s: Filebot didn't return an original filename", orig_fn)
