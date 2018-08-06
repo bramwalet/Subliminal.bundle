f31d75618546c6d92b86f311c388dd26a9c8c7e9
panni <panni@fragstore.net>
2018-08-06 15:57:39 +0200

    submod: HI: only remove caps before colon if the colon is followed by whitespace or EOL; fixes #542

--- Contents/Libraries/Shared/subzero/modification/mods/hearing_impaired.py
+++ Contents/Libraries/Shared/subzero/modification/mods/hearing_impaired.py
@@ -49,7 +49,7 @@ class HearingImpaired(SubtitleTextModification):
         # uppercase text before colon (at least 3 uppercase chars); at start or after a sentence,
         # possibly with a dash in front; ignore anything ending with a quote
         NReProcessor(re.compile(ur'(?u)(?:(?<=^)|(?<=[.\-!?\"\']))([\s-]*(?=[A-ZÀ-Ž&+]\s*[A-ZÀ-Ž&+]\s*[A-ZÀ-Ž&+])'
-                                ur'[A-ZÀ-Ž-_0-9\s\"\'&+]+:(?![\"\'’ʼ❜‘‛”“‟„])\s*)(?![0-9])'), "",
+                                ur'[A-ZÀ-Ž-_0-9\s\"\'&+]+:(?![\"\'’ʼ❜‘‛”“‟„])(?:\s+|$))(?![0-9])'), "",
                      name="HI_before_colon_caps"),
 
         # any text before colon (at least 3 chars); at start or after a sentence,
