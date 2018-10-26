c4a79c0e3767d7b9b85ca4ef1f99ea084e00cd88
panni <panni@fragstore.net>
2018-10-26 03:37:19 +0200

    core: add furl==2.0.0

---
+++ Contents/Libraries/Shared/furl/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+#
+# furl - URL manipulation made simple.
+#
+# Ansgar Grunseid
+# grunseid.com
+# grunseid@gmail.com
+#
+# License: Build Amazing Things (Unlicense)
+#
+
+from .furl import *  # noqa
+
+__title__ = 'furl'
+__version__ = '2.0.0'
+__license__ = 'Unlicense'
+__author__ = 'Ansgar Grunseid'
+__contact__ = 'grunseid@gmail.com'
+__url__ = 'https://github.com/gruns/furl'
