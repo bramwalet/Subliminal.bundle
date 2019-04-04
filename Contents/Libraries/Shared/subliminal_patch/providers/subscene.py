20f18fc3911a2930f560eac8b9d1ea7ae00462c3
panni <panni@fragstore.net>
2019-04-04 16:45:53 +0200

    providers: subscene: try reusing cf cookies after successful bypass

--- Contents/Libraries/Shared/subliminal_patch/providers/subscene.py
+++ Contents/Libraries/Shared/subliminal_patch/providers/subscene.py
@@ -13,7 +13,9 @@ from zipfile import ZipFile
 from babelfish import language_converters
 from guessit import guessit
 from requests import Session
+from dogpile.cache.api import NO_VALUE
 from subliminal import Episode, ProviderError
+from subliminal.cache import region
 from subliminal.utils import sanitize_release_group
 from subliminal_patch.providers import Provider
 from subliminal_patch.providers.mixins import ProviderSubtitleArchiveMixin
@@ -200,7 +202,23 @@ class SubsceneProvider(Provider, ProviderSubtitleArchiveMixin):
         subtitles = []
         logger.debug(u"Searching for: %s", vfn)
 
+        cf_data = region.get("cf_data")
+        if cf_data is not NO_VALUE:
+            cf_cookies, user_agent = cf_data
+            logger.debug("Trying to use old cf cookies")
+            self.session.cookies.update(cf_cookies)
+            self.session.headers['User-Agent'] = user_agent
+
         film = search(vfn, session=self.session)
+
+        try:
+            cf_data = self.session.get_live_tokens("subscene.com")
+        except:
+            pass
+        else:
+            logger.debug("Storing cf cookies")
+            region.set("cf_data", cf_data)
+
         if film and film.subtitles:
             logger.debug('Release results found: %s', len(film.subtitles))
             subtitles = self.parse_results(video, film)
