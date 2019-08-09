527d171a6a7d95305ab3e2322ef6aac7d11e2f17
viking1304 <viking1304@gmail.com>
2019-08-09 18:54:35 +0200

    Disable provider Titlovi if user and password are not set

--- Contents/Code/support/config.py
+++ Contents/Code/support/config.py
@@ -762,7 +762,9 @@ class Config(object):
                      # 'thesubdb': Prefs['provider.thesubdb.enabled'],
                      'podnapisi': cast_bool(Prefs['provider.podnapisi.enabled']),
                      'napisy24': cast_bool(Prefs['provider.napisy24.enabled']),
-                     'titlovi': cast_bool(Prefs['provider.titlovi.enabled']),
+                     'titlovi': cast_bool(Prefs['provider.titlovi.enabled']) and
+                                cast_bool(Prefs['provider.titlovi.username']) and
+                                cast_bool(Prefs['provider.titlovi.password']),
                      'addic7ed': cast_bool(Prefs['provider.addic7ed.enabled']) and self.has_anticaptcha,
                      'tvsubtitles': cast_bool(Prefs['provider.tvsubtitles.enabled']),
                      'legendastv': cast_bool(Prefs['provider.legendastv.enabled']),
